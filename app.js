3669571e43895656b0bfd9cded569a8a6217010b
AaronDai1113 <aaron11388@outlook.com>
2020-09-17 16:37:03 +0800

    small

--- app.js
+++ app.js
@@ -10,16 +10,23 @@ for(i=0;i<boxNumber;i++){
 }
 
 function setCircle(i){
-    document.getElementsByClassName('circle')[i].style.backgroundColor='#aaa'
+    document.getElementsByClassName('circle')[i].style.backgroundColor=randomColor()
     document.getElementsByClassName('circle')[i].style.width='70px'
     document.getElementsByClassName('circle')[i].style.height='70px'
     document.getElementsByClassName('circle')[i].style.top='50px'
     document.getElementsByClassName('circle')[i].style.left='30px'
 }
 function setSquare(i){
-    document.getElementsByClassName('square')[i].style.backgroundColor='#ddd'
+    document.getElementsByClassName('square')[i].style.backgroundColor=randomColor
     document.getElementsByClassName('square')[i].style.width='30px'
     document.getElementsByClassName('square')[i].style.height='30px'
     document.getElementsByClassName('square')[i].style.top='10px'
     document.getElementsByClassName('square')[i].style.left='10px'
+}
+
+function randomColor(){
+    let colors=["#ffcc22","#aadd22","#ccbb11","#23bdac"]
+    let counts= colors.length
+    let n=Math.ceil(Math.random()*counts)
+    return colors[n]
 }
